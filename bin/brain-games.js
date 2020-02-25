3d4fa7caafd4ebae161af3d1c5689d6cec545b1f
Pavel Sirotkin <pav.pnz@gmail.com>
2020-02-25 22:55:29 +0100

    f

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games');
+
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
 
