983fdb2993920f11de980e0aab8abfdb18247b39
Pavel Sirotkin <pav.pnz@gmail.com>
2020-02-26 21:44:44 +0100

    linter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const sayHello = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
 };
 
 export default sayHello;
