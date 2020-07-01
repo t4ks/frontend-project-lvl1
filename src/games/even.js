c6a3cb91cabc850f064b965ca208ca1513271d6c
Pavel Sirotkin <pav.pnz@gmail.com>
2020-07-01 19:50:09 +0200

    f

--- src/games/even.js
+++ src/games/even.js
@@ -3,7 +3,6 @@ import { getRandomNumber } from '../utils.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-
 export default () => runGame(() => {
   const question = getRandomNumber(1, 100);
   const correctAnswer = (question % 2 === 0) ? 'yes' : 'no';
--- src/games/progression.js
+++ src/games/progression.js
@@ -5,24 +5,15 @@ const generateProgression = (step, length, firstElement) => Array(length)
   .fill(firstElement)
   .map((currentValue, index) => currentValue + (step * index));
 
-
 const PROGRESSION_LEN = 10;
 const description = 'What number is missing in the progression?';
 
-const progression = () => {
+export default () => runGame(() => {
   const step = getRandomNumber(2, 6);
   const firstElementInProgression = getRandomNumber(0, 100);
-  const randomProgression = generateProgression(step, PROGRESSION_LEN, firstElementInProgression);
-  const randomIndexInProgression = getRandomNumber(0, PROGRESSION_LEN);
-  let correctAnswer;
-  if (randomIndexInProgression === randomProgression.length) {
-    correctAnswer = randomProgression[randomIndexInProgression - 1];
-    randomProgression[randomIndexInProgression - 1] = '..';
-  } else {
-    correctAnswer = randomProgression[randomIndexInProgression];
-    randomProgression[randomIndexInProgression] = '..';
-  }
-  return { question: randomProgression.join(' '), correctAnswer: correctAnswer.toString() };
-};
-
-export default () => runGame(progression, description);
+  const progression = generateProgression(step, PROGRESSION_LEN, firstElementInProgression);
+  const randomIndexInProgression = getRandomNumber(0, PROGRESSION_LEN - 1);
+  const correctAnswer = progression[randomIndexInProgression];
+  progression[randomIndexInProgression] = '..';
+  return { question: progression.join(' '), correctAnswer: correctAnswer.toString() };
+}, description);
